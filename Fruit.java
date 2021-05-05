ca11c0e83f49b72fe569dae0f8c0a30d41e91ce7
adilahdiyana <81138230+adilahdiyana@users.noreply.github.com>
2021-05-05 23:25:58 +0800

    Create Fruit.java

---
+++ Fruit.java
@@ -0,0 +1,11 @@
+package Exercise4_4;
+
+public class Fruit { //super class
+
+	protected String name;
+	
+	public Fruit(String name) {
+		this.name = name;
+		System.out.println("Fruit constructor is invoked.");
+	}
+} //end for class Fruit
