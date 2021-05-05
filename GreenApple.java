348ca6091c10d84a2f32d4087dc9090b98a4e72d
adilahdiyana <81138230+adilahdiyana@users.noreply.github.com>
2021-05-05 23:27:15 +0800

    Create GreenApple.java

---
+++ GreenApple.java
@@ -0,0 +1,27 @@
+package Exercise4_4;
+
+public class GreenApple extends Apple { //sub class for Apple
+	
+	protected String brand;
+	protected double vitc;
+	
+	public GreenApple(String name, double price, int quantity, String brand, double vitc) { 
+		super(name, price, quantity);
+		this.brand = brand;
+		this.vitc = vitc;
+		
+		System.out.println("Green Apple constructor is invoked.");
+	}
+
+	public String brandGreenApple() {
+		return this.brand;
+	}
+	
+	public double vitc() {
+		return this.vitc;
+	}
+		
+	public double calPrice() { //overriding method from class Apple
+		return super.price*super.quantity;
+	}
+} //end for class GreenApple
